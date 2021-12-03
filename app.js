fa70c6d753ca359cf42236c8b278166e3d1e61e2
JustinLeung1 <justinleung1999@hotmail.com>
2021-12-02 21:14:59 -0800

    sign up working

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mysql = require('mysql2');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
+var config = require('./services/config');
+var connection = mysql.createConnection(config.db);
+var session = require('express-session');
+
+global.db = connection;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,7 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  }))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
---
+++ services/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db : {
+        "host": "localhost",
+        "user":"root",
+        "password":"password",
+        "database":"CS174"
+    }
+};
+
+module.exports = config;
